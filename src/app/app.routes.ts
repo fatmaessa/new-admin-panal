56d612f210eb235cb2054040f9b26f2097c35c80
fatma essa <fatmaessa341@gmail.com>
2026-03-14 18:32:38 +0200

    "log_in page v1"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LogInPage} from './log-in-page/log-in-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path:"login", component: LogInPage}
+];
---
+++ src/app/log-in-page/log-in-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-page',
+  imports: [],
+  templateUrl: './log-in-page.html',
+  styleUrl: './log-in-page.scss',
+})
+export class LogInPage {
+
+}
