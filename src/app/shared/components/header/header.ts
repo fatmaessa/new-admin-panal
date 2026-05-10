b3cc411dd1adb1a357a4ff40a4f1e7e3226814c2
fatma essa <fatmaessa341@gmail.com>
2026-05-10 22:09:39 +0300

    search component

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth/auth-service';
+import { SearchComponent } from "../search/search";
+import { SearchResult } from '../../models/search_models';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+$result: any;
+
   authService = inject(AuthService);
 
   logout() {
